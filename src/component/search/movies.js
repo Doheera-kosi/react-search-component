77a9c6ce2eec404af25055d99169971d0ef08db3
Doheera-kosi <kupourevans99@gmail.com>
2023-03-20 02:37:10 +0000

    Update movies.js

--- src/component/search/movies.js
+++ src/component/search/movies.js
@@ -1,13 +1,13 @@
 const movies = [
-  { title: 'The Godfather', year: 1972 },
-  { title: 'The Shawshank Redemption', year: 1994 },
-  { title: 'The Dark Knight', year: 2008 },
-  { title: '12 Angry Men', year: 1957 },
-  { title: 'Schindler\'s List', year: 1993 },
-  { title: 'Downton Abbey', year: 2019 },
-  { title: 'Charlie Bartlett', year: 2008 },
-  { title: 'A Beautiful Day in the Neighborhood', year: 2019 },
-  { title: 'Edge of Tomorrow', year: 2014 },
+  { title: 'The Godfather ', year: 1972 },
+  { title: 'The Shawshank Redemption  ', year: 1994 },
+  { title: 'The Dark Knight ', year: 2008 },
+  { title: '12 Angry Men  ', year: 1957 },
+  { title: 'Schindler\'s List ', year: 1993 },
+  { title: 'Downton Abbey ', year: 2019 },
+  { title: 'Charlie Bartlett  ', year: 2008 },
+  { title: 'A Beautiful Day in the Neighborhood ', year: 2019 },
+  { title: 'Edge of Tomorrow  ', year: 2014 },
 ];
 
 export default movies;
